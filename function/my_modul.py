c6bf08156e6b01d134c7a24eb1bb01f6eb55cbf8
GMNocK <userwith.immortal@gmail.com>
2023-06-20 15:15:54 +0700

    day 7 : mastering function - lambda & anonymous function

--- function/my_modul.py
+++ function/my_modul.py
@@ -50,9 +50,73 @@ def aritmatika(*nums:int, **operator) -> int:
         return 0
 
 print(aritmatika(1,2,3,4,operator="kali"))
-        
 
-        
-        
-    
-aritmatika(1,2,3,4,5,6, operator="tambah")
+
+#  LAMBDA FUNCTION
+def f_kuadrat(angka:int) ->int :
+    return angka**2
+
+print(f"hasil fungsi kuadrat : {f_kuadrat(3)}")
+
+kuadrat = lambda angka: angka**2
+print(f"hasil lambda kuadrat : {kuadrat(3)}")
+
+kuadrat = lambda angka,pow: angka**pow
+print(f"hasil lambda kuadrat #2 : {kuadrat(3,3)}")
+
+
+# case - list sort
+data_list = ["Yukie","Dudung", "Asep"]
+data_list.sort()
+print(data_list)
+
+# sort berdasarkan panjang
+data_list.sort(key=len)
+print(data_list)
+
+data_list.sort(key=lambda nama:len(nama))
+print(f"sorted list by lambda : {data_list}")
+
+
+data_angka = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+def kurang_dari_lima(angka) :
+    return angka < 5
+data_angka_baru = list(filter(kurang_dari_lima, data_angka))
+print(data_angka_baru)
+# kasus genap
+
+data_angka_baru = list(filter(lambda x:x<5, data_angka))
+print(f"Filter dengan lambda : {data_angka_baru}")
+
+
+data_genap = list(filter(lambda x:(x%2==0),data_angka))
+print(f"Data genap lambda : {data_genap}")
+
+data_ganjil = list(filter(lambda x:(x%2!=0),data_angka))
+print(f"Data ganjil lambda : {data_ganjil}")
+
+data_3 = list(filter(lambda x:(x%3==0),data_angka))
+print(f"Data kelipatan 3 lambda : {data_3}")
+
+data_4 = list(filter(lambda x:(x%4==0),data_angka))
+print(f"Data kelipatan 4 lambda : {data_4}")
+
+
+
+# Anonymous function
+# currying <- haskel Curry
+print(f"\nAnonymous")
+def pangkat(angka,n) :
+    return angka**n 
+
+print(pangkat(5,2))
+
+def pangkat(n) :
+    return lambda angka:angka**n
+
+pangkat2 = pangkat(2)
+print(f"Pangkat 2 : {pangkat2(5)}")
+pangkat3 = pangkat(3)
+print(f"Pangkat 3 : {pangkat3(5)}")
+print(f"Pangkat bebas : {pangkat(4)(5)}")
