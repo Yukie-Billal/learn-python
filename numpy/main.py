1553b0d05245ca9d088611e6f223c122e4f9da37
GMNocK <userwith.immortal@gmail.com>
2023-06-23 13:46:33 +0700

    day 8 : numpy contoh module pip

---
+++ numpy/main.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+list_a = [1,2,3,4]
+vector_a = np.array([1,2,3,4])
+print(f"List a : {list_a}")
+print(f"Vector a : {vector_a}")
+
+
+# print(f"List : {list_a**2}") # <- Gagal
+print(f"Vector : {vector_a**2}")
+print(f"List : {list_a*2}")
+print(f"Vector : {vector_a*2}")
